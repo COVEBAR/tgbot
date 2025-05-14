1cded660cebd77d968dab55faa4b2b192035fe5d
COVEBAR <ivashkam113@gmail.com>
2025-05-14 11:13:13 +0700

    fin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, json, telebot, sqlite3, datetime, random
+import requests, json, telebot, sqlite3, datetime, randomadd
 
 msg_time = datetime.datetime.now()
 
@@ -22,6 +22,38 @@ def get_news():
         msg_time = datetime.datetime.now()
         return news[0]
 
+def math_game():
+    problem = random.randint(0, 3)
+    if problem == 0:
+        num1 = random.randint(0, 100)
+        num2 = random.randint(0, 100)
+        problem = f"{num1} + {num2}"
+        sol = num1 + num2
+    elif problem == 1:
+        num1 = random.randint(0, 30)
+        num2 = random.randint(0, 30)
+        problem = f"{num1} * {num2}"
+        sol = num1 * num2
+    elif problem == 2:
+        num2 = random.randint(1, 50)
+        num1 = num2 * random.randint(1, 10)
+        problem = f"{num1} / {num2}"
+        sol = num1 // num2
+    elif problem == 3:
+        num1 = random.randint(0, 100)
+        num2 = random.randint(0, 99)
+        while num2 > num1:
+            num2 = random.randint(0, 99)
+        problem = f"{num1} - {num2}"
+        sol = num1 - num2
+
+    wrong = random.randint(0, 100)
+    while wrong == sol:
+        wrong = random.randint(0, 100)
+    return (problem, sol, wrong)
+
+
+#тг бот
 bot = telebot.TeleBot("8181972121:AAG1cSkgwIRlQAwiiWed4hMHaN5e-iFj9DM")
 
 # превед
@@ -37,6 +69,7 @@ def send_help(message):
     bot.send_message(message.chat.id, """ВЕЛИКАЯ, НЕПОВТОРИМАЯ И ОПРЕДЕЛЁННО ТОЧНО НЕ БАКА, СЫРНО, МОЖЕТ СДЕЛАТЬ ДЛЯ ТЕБЯ СЛЕДУЮЩЕЕ:
 /gifka - отправить тохо-гифку
 /news - рассказать последние новости в Генсокё
+/math - задать математический пример
 НИ В КОЕМ СЛУЧАЕ НЕ НАЗЫВАЙ МЕНЯ "БАКА"!!!""")
 
 # гиф
@@ -49,11 +82,38 @@ def send_gif(message):
 def send_news(message):
     bot.send_message(message.chat.id, get_news())
 
+#матеша
+@bot.message_handler(commands=['math'])
+def math(message):
+    # создаём задачку и ответы - правильный (неправильный) и рандомный неправильный (правильный)
+    problem, f_solution, r_solution = math_game()
+
+    if random.randint(0, 1) == 1:
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton(f'{f_solution}', callback_data="fs"))
+        markup.add(telebot.types.InlineKeyboardButton(f'{r_solution}', callback_data="ts"))
+        bot.send_message(message.chat.id, f"""Вот тебе пример, который ты никогда не сможешь правильно решить:
+{problem}""", reply_markup=markup)
+    else:
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton(f'{r_solution}', callback_data="ts"))
+        markup.add(telebot.types.InlineKeyboardButton(f'{f_solution}', callback_data="fs"))
+        bot.send_message(message.chat.id, f"""Вот тебе пример, который ты никогда не сможешь правильно решить:
+{problem}""", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def math_check(callback):
+    if callback.data == "fs":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        bot.send_message(callback.message.chat.id, "Ха-ха-ха! Я же говорила, что ты никогда не сможешь его правильно решить! БАКА!")
+    elif callback.data == "ts":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        bot.send_message(callback.message.chat.id, "Уф, а ты куда умнее, чем я изначально думала...")
+
 # реакт на "бака"
 @bot.message_handler()
 def baka_check_status(message):
     if "бака" in message.text.lower() or "baka" in message.text.lower():
         bot.send_message(message.chat.id, """ВООБЩЕ-ТО Я ОЧЕНЬ УМНАЯ! ДА ТЫ САМ БАКА! ЧТО ВООБЩЕ ЗА ЧЕПУХУ ТЫ ГОРОДИШЬ ПРО МЕНЯ!""")
 
-# математика
 bot.infinity_polling()
